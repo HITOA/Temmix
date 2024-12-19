15f67bf068848daac070d26291706b854ed1fb66
HITOA <charli.poirier@gmail.com>
2024-12-19 17:19:17 +0100

    A very clean message for a very clean commit

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
 
   outputs = inputs @ { self, nixpkgs, lain-src, ... }:
   let
-    lain = pkgs.callPackage (./lain.nix lain-src);
+    lain = import (./lain.nix lain-src);
   in
   {
     nixosModules.temmix = import ./temmix.nix lain;
