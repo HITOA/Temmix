1a76680929614fb402e0d26f6c485d48809fe881
HITOA <charli.poirier@gmail.com>
2024-12-21 16:45:59 +0100

    fix?

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "A thmeing packaegs.";
 
   inputs = {
-    /*home-manager = {
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-    };*/
+    };
 
     lain-src = {
       url = "github:HITOA/Lain";
@@ -15,7 +15,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = inputs @ { self, nixpkgs, lain-src, ... }:
+  outputs = { self, nixpkgs, lain-src, ... }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
