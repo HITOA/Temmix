16460565e6abe776f531aa2bccbf7a003af52a34
HITOA <charli.poirier@gmail.com>
2024-12-23 23:26:32 +0100

    kitty theme!

---
+++ modules/terminal/default.nix
@@ -0,0 +1,25 @@
+{ lib, config, pkgs, ... } :
+let
+  	renderedTemplatePath = config.temmix.cacheFile + "/set-terminal-color.sh";
+in
+{
+	options.temmix.terminal = {
+		enable = lib.mkOption {
+			type = lib.types.bool;
+			description = "Enable theming for Any Terminal.";
+			default = false;
+		};
+	};
+
+	config = lib.mkIf (config.temmix.enable && config.temmix.terminal.enable)
+	{
+		config.temmix.commands = [''
+      source ${renderedTemplatePath}
+    ''];
+
+		temmix.templates = [{ 
+			input = ./set-terminal-color.sh.inja; 
+			output =  renderedTemplatePath;
+		}];
+	};
+}
