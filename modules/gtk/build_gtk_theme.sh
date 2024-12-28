c48529061d3e7640e921089a90faeec825656a32
HITOA <charli.poirier@gmail.com>
2024-12-28 23:31:38 +0100

    gtk test

--- modules/gtk/build_gtk_theme.sh
+++ modules/gtk/build_gtk_theme.sh
@@ -2,4 +2,4 @@ export PATH="$coreutils/bin"
 
 mkdir -p $out/share/themes
 cp -r $src $out/share/themes/temmix
-ln -s $color $out/share/themes/temmix/gtk-3.0/gtk-color.css
+ln -s $color $out/share/themes/gtk-color.css
